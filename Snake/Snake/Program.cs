18dc7ab802a2b979f0554d5eb3e19670a7c77d67
Ivan Tulinov <energiay@gmail.com>
2015-09-30 15:36:38 +0300

    Эксперемент с типами данных. Наконец я доконца понял, что значит ссылочный тап данных.

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,13 +10,31 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(3,4,'*');
+            Point p1 = new Point(1,1,'*');
             p1.Show();
 
-            Point p2 = new Point(4,5,'#');
-            p2.Show();
+            Function(p1);
+
+            p1.Show();
+
+            //Point p2 = new Point(4,5,'#');
+            //p2.Show();
 
             Console.ReadKey();
+        }
+
+        public static void Function (Point p)
+        {
+            p.sym = '2';
+
+            p.Show();
+
+            p.x = 1;
+            p.y = 3;
+            p.sym = '3';
+            p = new Point(1,4,'4');
+            p.Show();
+
         }
     }
 }
